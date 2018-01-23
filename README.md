98c945afb4afee7450cb86313010fb35b9576185
Sayful Islam <sayful.islam001@gmail.com>
2018-01-23 16:02:12 +0600

    Initial files.

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# slim-flash
-Easy flash notifications using sweetAlert for Slim Framework 3
+# Easy Flash Messages for Slim Framework 3
+
+This repository contains a Slim Framework Flash messages service provider. This enables you to define transient messages that persist only from the current request to the next request.
+
+## Install
+
+Via Composer
+
+``` bash
+$ composer require sayful1/slim-flash
+```
+
+Requires Slim 3.0.0 or newer.
+
+## Usage
+
+```php
+// Start PHP session
+session_start();
+
+$app = new \Slim\App();
+
+// Fetch DI Container
+$container = $app->getContainer();
+
+// Register provider
+$container['flash'] = function () {
+    return new \Sayful\SlimFlash\Flash();
+};
+
+$app->get('/foo', function ($req, $res, $args) {
+    // Set flash message for next request
+    $this->flash->success('Test', 'This is a message');
+
+    // Redirect
+    return $res->withStatus(302)->withHeader('Location', '/bar');
+});
+
+$app->get('/bar', function ($req, $res, $args) {
+    // Get flash messages from previous request
+    $messages = $this->flash->getMessages();
+    print_r($messages);
+
+    // Get the first message
+    $test = $this->flash->getFlashMessage();
+    print_r($test);
+});
+
+$app->run();
+```
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ src/Flash.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace Sayful\SlimFlash;
+
+
+class Flash extends FlashMessages {
+
+	/**
+	 * Create a new flash notifier service provider.
+	 *
+	 * @param null|array|\ArrayAccess $storage
+	 * @param null|string $storageKey
+	 */
+	public function __construct( &$storage = null, $storageKey = null ) {
+		parent::__construct( $storage, $storageKey );
+	}
+
+	/**
+	 * Get flash message
+	 *
+	 * @return mixed
+	 */
+	public function getFlashMessage() {
+		return $this->getFirstMessage( 'flash_message' );
+	}
+
+	/**
+	 * Get overlay message
+	 *
+	 * @return mixed
+	 */
+	public function getOverlayMessage() {
+		return $this->getFirstMessage( 'flash_message_overlay' );
+	}
+
+	/**
+	 * Create flash message
+	 *
+	 * @param  string $title
+	 * @param  string $message
+	 * @param  string $label
+	 * @param  string|null $key
+	 *
+	 * @return void
+	 */
+	public function create( $title, $message, $label, $key = 'flash_message' ) {
+		$this->addMessage( $key, [
+			'title'   => $title,
+			'message' => $message,
+			'label'   => $label,
+		] );
+	}
+
+	/**
+	 * Create an overlay flash message
+	 *
+	 * @param  string $title
+	 * @param  string $message
+	 * @param  string|null $label
+	 *
+	 * @return void
+	 */
+	public function overlay( $title = null, $message = null, $label = 'success' ) {
+		$num_args = func_num_args();
+		$title    = $num_args >= 1 ? func_get_arg( 0 ) : null;
+		$message  = $num_args >= 2 ? func_get_arg( 1 ) : null;
+		$label    = $num_args >= 3 ? func_get_arg( 2 ) : 'success';
+		$this->create( $title, $message, $label, 'flash_message_overlay' );
+	}
+
+	/**
+	 * Create an success flash message
+	 *
+	 * @param  string $title
+	 * @param  string $message
+	 *
+	 * @return void
+	 */
+	public function success( $title = null, $message = null ) {
+		$num_args = func_num_args();
+		$title    = $num_args >= 1 ? func_get_arg( 0 ) : null;
+		$message  = $num_args >= 2 ? func_get_arg( 1 ) : null;
+		$this->create( $title, $message, 'success' );
+	}
+
+	/**
+	 * Create an info flash message
+	 *
+	 * @param  string $title
+	 * @param  string $message
+	 *
+	 * @return void
+	 */
+	public function info( $title = null, $message = null ) {
+		$num_args = func_num_args();
+		$title    = $num_args >= 1 ? func_get_arg( 0 ) : null;
+		$message  = $num_args >= 2 ? func_get_arg( 1 ) : null;
+		$this->create( $title, $message, 'info' );
+	}
+
+	/**
+	 * Create an warning flash message
+	 *
+	 * @param  string $title
+	 * @param  string $message
+	 *
+	 * @return void
+	 */
+	public function warning( $title = null, $message = null ) {
+		$num_args = func_num_args();
+		$title    = $num_args >= 1 ? func_get_arg( 0 ) : null;
+		$message  = $num_args >= 2 ? func_get_arg( 1 ) : null;
+		$this->create( $title, $message, 'warning' );
+	}
+
+	/**
+	 * Create an error flash message
+	 *
+	 * @param  string $title
+	 * @param  string $message
+	 *
+	 * @return void
+	 */
+	public function error( $title = null, $message = null ) {
+		$num_args = func_num_args();
+		$title    = $num_args >= 1 ? func_get_arg( 0 ) : null;
+		$message  = $num_args >= 2 ? func_get_arg( 1 ) : null;
+		$this->create( $title, $message, 'error' );
+	}
+}
